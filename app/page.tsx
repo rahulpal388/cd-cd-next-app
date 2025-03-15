aa7957059089ca404b532daac38a4529893a5ecd
rahulpal388 <rahulschoolmail59@gmail.com>
2025-03-15 11:44:15 +0530

    initial code

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+
+
+export default function Home() {
+  return <div className="h-screen w-screen flex justify-center items-center ">
+    <h1 className=" text-6xl font-medium  ">Rahul Pal</h1>
+  </div>
+}
