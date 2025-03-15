f61feb7bdbb70dc478f6c0c1f627d9950c0b9de4
rahulpal388 <rahulschoolmail59@gmail.com>
2025-03-15 13:23:33 +0530

    added name

--- app/page.tsx
+++ app/page.tsx
@@ -4,5 +4,6 @@
 export default function Home() {
   return <div className="h-screen w-screen flex justify-center items-center ">
     <h1 className=" text-6xl font-medium  ">Rahul Pal</h1>
+    <h1 className=" text-6xl font-medium  ">Ritesh Pal</h1>
   </div>
 }
